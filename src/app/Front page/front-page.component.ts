98e5c21ce5c62871fdb8b291053188077f0cae09
Viktoras Jegorovas <viktoras.jego@gmail.com>
2017-09-01 20:07:10 +0300

    Preview UI

--- src/app/Front page/front-page.component.ts
+++ src/app/Front page/front-page.component.ts
@@ -15,15 +15,16 @@ export class FrontPageComponent implements OnInit, AfterViewInit {
     public userInfo: any = {fullName: '', profilePicture: '', username: ''};
     public status: string = '';
     public dummyPhotos = {
-        0: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/sh0.08/e35/p640x640/20066799_706414102898617_3184857248010600448_n.jpg',
-        1: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/sh0.08/e35/p640x640/20184919_116943978935194_8567338812005416960_n.jpg',
-        2: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/sh0.08/e35/p640x640/20185014_286653648475373_1163594412438061056_n.jpg',
-        3: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/sh0.08/e35/p640x640/20214307_329021344190130_4159934563595845632_n.jpg',
-        4: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/sh0.08/e35/p640x640/20398961_122189688403027_2017131716983914496_n.jpg',
-        5: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/sh0.08/e35/p640x640/20394388_1434919299926135_5052655095301275648_n.jpg',
-        6: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/sh0.08/e35/p640x640/20481786_1422217437861803_1942832815737405440_n.jpg',
-        7: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/e35/20590310_471501456556266_1623176528953081856_n.jpg',
-        8: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/sh0.08/e35/p640x640/20688322_292010481266899_7157904075021877248_n.jpg'}
+        0: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s150x150/e35/c0.108.873.873/20066799_706414102898617_3184857248010600448_n.jpg',
+        1: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s150x150/e35/c0.107.861.861/20184919_116943978935194_8567338812005416960_n.jpg',
+        2: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s150x150/e35/c0.99.797.797/20185014_286653648475373_1163594412438061056_n.jpg',
+        3: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s150x150/e35/c0.134.1080.1080/20214307_329021344190130_4159934563595845632_n.jpg',
+        4: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s150x150/e35/c0.100.799.799/20398961_122189688403027_2017131716983914496_n.jpg',
+        5: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s150x150/e35/c0.103.827.827/20394388_1434919299926135_5052655095301275648_n.jpg',
+        6: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s150x150/e35/c0.135.1080.1080/20481786_1422217437861803_1942832815737405440_n.jpg',
+        7: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s150x150/e35/c0.62.595.595/20590310_471501456556266_1623176528953081856_n.jpg',
+        8: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s150x150/e35/c0.99.797.797/20688322_292010481266899_7157904075021877248_n.jpg'
+    };
     public dummyUser = {
         fullName: 'Marius Pilipas',
         profilePicture: 'https://scontent-iad3-1.cdninstagram.com/t51.2885-19/s150x150/14591986_1052414564904497_290196869877858304_n.jpg',
@@ -59,6 +60,7 @@ export class FrontPageComponent implements OnInit, AfterViewInit {
             dataType: 'jsonp',
             type: 'GET',
             success: (data) => {
+                console.log(data);
                 this.getData(data);
             },
             error: function(error){
@@ -73,7 +75,7 @@ export class FrontPageComponent implements OnInit, AfterViewInit {
         if (0 < data.items.length) {
             // Gets Images
             data.items.forEach((item) => {
-                this.images.unshift(item.images.standard_resolution.url);
+                this.images.unshift(item.images.thumbnail.url);
             });
             this.userInfo.fullName = data.items[0].user.full_name;
             this.userInfo.profilePicture = data.items[0].user.profile_picture;
